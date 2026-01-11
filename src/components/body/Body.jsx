046933cfce2164406eb2c3c869fabc28d33428ee
pranjal499 <pranjalprofsocial72@gmail.com>
2026-01-11 23:29:26 +0530

    header and body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,81 +1,84 @@
-export default function Body() {
+function App() {
     return (
-        <div className="w-[55rem] mx-auto flex flex-wrap">
+        <div className="w-[55rem] mx-auto flex flex-wrap text-black">
 
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start w-[40%] mb-2 h-3">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start w-[58%] ml-auto mb-2">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start w-[100%]">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
-            <div className="bg-[#e2d06f] border-4 border-white p-6 min-h-[140px] flex items-start">
-                <div className="flex flex-col gap-2.5">
-                    <span className="text-xl font-bold">01</span>
-                    <p className="m-0 text-base leading-[1.4] font-semibold">
-                        Do projects need to be related to the theme?
-                    </p>
-                </div>
-            </div>
 
-            
+            <div className="bg-[#E4C653] border-4 border-white p-6 min-h-[140px] flex items-start w-[40%] mb-2 h-[12rem]">
+                <div className="flex flex-col gap-2.5 m-auto">
+                    <span className="text-xl font-bold">02</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="bg-[#7CA7D2] border-4 border-white p-6 min-h-[140px] flex items-start w-[58%] ml-auto mb-2">
+                <div className="flex flex-col gap-2.5 m-auto">
+                    <span className="text-xl font-bold">03</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="bg-[#536281] border-4 border-white p-6 min-h-[140px] flex items-start w-[100%] [clip-path:polygon(0%_0%,100%_0%,100%_92%,0%65%)] mb-10 h-[17rem] -mb-[5rem]">
+                <div className="flex flex-col gap-2.5 border-black mx-auto my-[3rem] ">
+                    <span className="text-xl font-bold">04</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="bg-[#77A2CF] border-4 border-white p-6 min-h-[140px] flex items-start [clip-path:polygon(0%_0%,100%_6%,100%_94%,0%100%)] w-[30%] h-[26.5rem] -mb-[4rem]">
+                <div className="flex flex-col gap-2.5 m-auto">
+                    <span className="text-xl font-bold">05</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="h-[25rem] w-[67%] ml-auto -mb-[4rem]">
+                <div className="bg-[#DABB3F] border-4 border-white p-6 min-h-[140px] flex items-start h-[45%] mt-[1.5rem] [clip-path:polygon(0%_0%,100%_29%,100%_100%,0%100%)] mb-2">
+                    <div className="flex flex-col gap-2.5 m-auto">
+                        <span className="text-xl font-bold">06</span>
+                        <p className="m-0 text-base leading-[1.4] font-semibold">
+                            Do projects need to be related to the theme?
+                        </p>
+                    </div>
+                </div>
+                <div className="bg-[#D2534A] border-4 border-white p-6 min-h-[140px] flex items-start h-[45%] mt-auto [clip-path:polygon(0%_0%,100%_0%,100%_68%,0%100%)]">
+                    <div className="flex flex-col gap-2.5">
+                        <span className="text-xl font-bold">07</span>
+                        <p className="m-0 text-base leading-[1.4] font-semibold">
+                            Do projects need to be related to the theme?
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-[#6F814F] border-4 border-white p-6 min-h-[140px] flex items-start w-[100%] [clip-path:polygon(0%_36%,100%_0%,100%_100%,0%100%)] h-[15rem] mb-2">
+                <div className="flex flex-col gap-2.5 m-auto">
+                    <span className="text-xl font-bold">08</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="bg-[#D5B437] border-4 border-white p-6 min-h-[140px] flex items-start w-[60%]">
+                <div className="flex flex-col gap-2.5">
+                    <span className="text-xl font-bold">09</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
+            <div className="bg-[#5986B3] border-4 border-white p-6 min-h-[140px] flex items-start w-[38%] ml-auto">
+                <div className="flex flex-col gap-2.5">
+                    <span className="text-xl font-bold">10</span>
+                    <p className="m-0 text-base leading-[1.4] font-semibold">
+                        Do projects need to be related to the theme?
+                    </p>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
+export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,24 @@
+import './Header.css';
+
 export default function Header() {
     return (
         <div className="w-[98vw]">
 
             <div className="w-[53rem] h-[26rem] border-4 border-white mx-auto my-6 flex">
-                <div className="relative">
+                <div className="relative burst">
                     <img 
                         src="images/burst.png" 
                         alt="Got question image" 
                         className="w-[22rem] h-[22rem] -rotate-[15.85deg] opacity-90 relative"
                     />
-                    <h3 className="w-[437.4px] h-[337.3px] top-[7rem] left-[9rem] -rotate-[22.93deg] opacity-100 text-2xl text-[#C94A2F] absolute">
+                    <h3 className="w-[437.4px] h-[337.3px] top-[7rem] left-[8rem] -rotate-[22.93deg] opacity-100 text-2xl text-[#C94A2F] absolute ">
                         Got Questions?
                     </h3>
                 </div>
                 <div className="my-[5rem] ml-16">
 
-                    <h1>FAQ</h1>
-                    <div className="w-[25rem] h-12 text-[0.7rem] text-[#C94A2F]">
+                    <h1 className="faq-heading">FAQ</h1>
+                    <div className="w-[25rem] h-12 text-[0.7rem] text-[#C94A2F] faq-info">
                         <p>Got questions about the hackathon?</p>
                         <p>We've got all the info you need to get started</p>
                         <br />
@@ -24,6 +26,9 @@ export default function Header() {
                         <p> Ping us anytime at team.gdg.jec@gmail.com  — we're all ears (and keyboards)! 👨‍💻👩‍💻</p>
                     </div>
                 </div>
+            </div>
+            <div className="title text-center -mt-[3.5rem] mb-[4rem]">
+                <h1 className=''>RAGNAROK</h1>
             </div>
         </div>
 
